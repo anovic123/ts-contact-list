ba45c9df583b650f1b4d5ca32be8ae3aa96fffad
anovic123 <105565287+anovic123@users.noreply.github.com>
2022-12-18 15:04:22 +0300

    fixed routes for gh-pages

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ export const USERS_URL = 'https://638f37a34ddca317d7f1c45f.mockapi.io/users';
 export const CONTACTS_URL = 'https://638f37a34ddca317d7f1c45f.mockapi.io/contact-list';
 
 export enum MyRoutes {
-  Home = '/',
-  Login = '/login',
-  Contacts = '/contacts'
+  Home = 'https://anovic123.github.io/',
+  Login = 'https://anovic123.github.io/login',
+  Contacts = 'https://anovic123.github.io/contacts'
 }
