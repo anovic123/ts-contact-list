08d8bc68a353b7c19d7437af4954d0a0b93e1be7
anovic123 <105565287+anovic123@users.noreply.github.com>
2022-12-25 20:26:34 +0300

    fixed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ export const USERS_URL = 'https://638f37a34ddca317d7f1c45f.mockapi.io/users';
 export const CONTACTS_URL = 'https://638f37a34ddca317d7f1c45f.mockapi.io/contact-list';
 
 export enum MyRoutes {
-  Home = '/',
+  Home = '',
   Login = '/login',
   Contacts = '/contacts'
 }
