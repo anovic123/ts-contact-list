17e73705602922f66c941adbdb1295a9c4cf1b10
anovic123 <105565287+anovic123@users.noreply.github.com>
2022-12-25 20:41:36 +0300

    fixed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ export const USERS_URL = 'https://638f37a34ddca317d7f1c45f.mockapi.io/users';
 export const CONTACTS_URL = 'https://638f37a34ddca317d7f1c45f.mockapi.io/contact-list';
 
 export enum MyRoutes {
-  Home = 'ts-redux-toolkit-contact-list/',
-  Login = 'ts-redux-toolkit-contact-list/login',
-  Contacts = 'ts-redux-toolkit-contact-list/contacts',
+  Home = '/',
+  Login = '/login',
+  Contacts = '/contacts',
 }
